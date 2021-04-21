16f703e86a7e4e9e86da92719f6d4cc5c4f94ece
popeye11 <xiaodongzhang1981@gmail.com>
2021-04-21 14:22:06 +0200

    add ip connection check

--- s7readApp.py
+++ s7readApp.py
@@ -8,8 +8,8 @@ import os
 ts_plc_update       =    2
 #ip upload sampling time [sec]:
 ts_ip_update        =    5
-# reboot sampling time [sec]:
-ts_reboot   =    2
+# reboot sampling time [min]:
+ts_reboot   =    20
 #  s71500 rack number:
 rackNo   =    0
 #  s71500 slot number:
@@ -48,10 +48,10 @@ organId = 2002
 devId   = "3001"            # device ID
 api_ip_upload = 'user'      # user name
 api_password  = 'user'      # password
-
 auth_url = "http://88.99.24.40:8088/api/auth/signin";
 optIn_url = "http://88.99.24.40:8088/api/optIn";
-  
+uploadFailCount =0
+upload_success = True
 def step_fun_plc():
     try:
         plc =s7MingfengLib.plcConnect(PLC_IP,rackNo,slotNo)
@@ -59,7 +59,7 @@ def step_fun_plc():
         scy1_Ids,scy1_data =s7MingfengLib.readIDs(DBTables.DBIDs,Data,1)
         scy2_Ids,scy2_data =s7MingfengLib.readIDs(DBTables.DBIDs,Data,2)
         scy3_Ids,scy3_data =s7MingfengLib.readIDs(DBTables.DBIDs,Data,3)
-        #print(scy1_data)
+        print(scy1_data)
         s7MingfengLib.on_publish(mqttClient,topic1,s7MingfengLib.Convert2SimchengForm(scy1_Ids, scy1_data) , 1)
         s7MingfengLib.on_publish(mqttClient,topic2,s7MingfengLib.Convert2SimchengForm(scy2_Ids, scy2_data) , 1)
         s7MingfengLib.on_publish(mqttClient,topic3,s7MingfengLib.Convert2SimchengForm(scy3_Ids, scy3_data) , 1)
@@ -68,25 +68,41 @@ def step_fun_plc():
 
      
 def step_fun_upload_ip():
+    global uploadFailCount
+    global upload_success 
     try:
         ipadr = s7MingfengLib.get_ip()
        # ngrok_adr = s7XinyingLib.get_ngrok_url()
         s7MingfengLib.ip_upload(auth_url,optIn_url,api_ip_upload,api_password,organId,devId,ipadr,ipadr)
+        upload_success = True
     except:
-        print("upload ip failed")
+        upload_success = False
+        uploadFailCount = uploadFailCount+1
+        print("upload ip failed "+str(uploadFailCount)+" of maximal 20 times")
+        if uploadFailCount > 20:
+            print ("reboot ...")
+            os.system('sudo reboot')
     #ip_upload(auth_url,optIn_url,api_ip_upload,api_password,organId,devId,ipadr)
      
 def step_fun_reboot():
     print ("reboot ...")
     os.system('sudo reboot')
 
-
-
+def step_upload_success_check():
+    global uploadFailCount
+    global upload_success
+    if upload_success is True:
+        uploadFailCount =0
+    else:
+        print("the ip communication is not avaliable !")
 
 if __name__ == "__main__":
+
     mqttClient = s7MingfengLib.on_mqtt_connect(mqtthost,mqttport,clientname,username,password)
     sched = s7MingfengLib.task_scheduler()
     sched.add_job(step_fun_plc, 'cron', second='*/'+str(ts_plc_update),hour='*',max_instances = int(1e25))
     sched.add_job(step_fun_upload_ip, 'cron', second='*/'+str(ts_ip_update),hour='*',max_instances = int(1e25))
-    sched.add_job(step_fun_reboot, 'cron', minute='*/'+str(ts_reboot),hour='*',max_instances = int(1e25))
+    sched.add_job(step_upload_success_check, 'cron', second='*/'+str(ts_ip_update),hour='*',max_instances = int(1e25))
+    
+    #sched.add_job(step_fun_reboot, 'cron', minute='*/'+str(ts_reboot),hour='*',max_instances = int(1e25))
     sched.start()
